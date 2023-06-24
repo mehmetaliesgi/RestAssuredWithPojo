4e18c58a197291b1b08ef3adca52e977b32bafca
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-06-24 16:12:35 +0300

    Other assertions added and test case finished.

--- untitled/src/test/java/RestAssuredPojo/PostRequestWithPojoTests.java
+++ untitled/src/test/java/RestAssuredPojo/PostRequestWithPojoTests.java
@@ -11,8 +11,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class PostRequestWithPojoTests extends BaseUrl {
+    int bookingid;
     @Test
     public void PostRequestPojo() {
 
@@ -21,18 +23,26 @@ public class PostRequestWithPojoTests extends BaseUrl {
         BookingDatesPojo bookingdates = new BookingDatesPojo("2023-01-01", "2023-01-05");
         BookingPojo requestBody = new BookingPojo("Mehmet Ali", "EŞGİ", 245, false, bookingdates, "Wi-Fi");
 
-        // 2 - If they want to you expected data, create expected data
-        RootPojo expectedData = new RootPojo(1, requestBody);
-
-        // 3- Do a request, save response and modify the response
+        // 2- Do a request, save response and modify the response
         Response response = given(spec)
                 .contentType(ContentType.JSON)
                 .when()
                 .body(requestBody)
                 .post("{pp1}");
         RootPojo responsePojo = response.as(RootPojo.class);
+        bookingid = responsePojo.getBookingid();
+
+        // 3 - If they want to you expected data, create expected data
+        RootPojo expectedData = new RootPojo(bookingid, requestBody);
 
         // 4- Doing assertions
-        Assert.assertEquals(expectedData.getBooking().getFirstname(), responsePojo.getBooking().getFirstname());
+        assertEquals(expectedData.getBookingid(), responsePojo.getBookingid());
+        assertEquals(expectedData.getBooking().getFirstname(), responsePojo.getBooking().getFirstname());
+        assertEquals(expectedData.getBooking().getLastname(), responsePojo.getBooking().getLastname());
+        assertEquals(expectedData.getBooking().getTotalprice(), responsePojo.getBooking().getTotalprice());
+        assertEquals(expectedData.getBooking().isDepositpaid(), responsePojo.getBooking().isDepositpaid());
+        assertEquals(expectedData.getBooking().getBookingdates().getCheckin(), responsePojo.getBooking().getBookingdates().getCheckin());
+        assertEquals(expectedData.getBooking().getBookingdates().getCheckout(), responsePojo.getBooking().getBookingdates().getCheckout());
+        assertEquals(expectedData.getBooking().getAdditionalneeds(), responsePojo.getBooking().getAdditionalneeds());
     }
 }
