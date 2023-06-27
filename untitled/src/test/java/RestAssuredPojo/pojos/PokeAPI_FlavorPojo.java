bbab1ca4a2949a65af75520ae627edd373b1fbd0
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-06-27 12:22:37 +0300

    FlavorPojo files was added the project using to lombok library

---
+++ untitled/src/test/java/RestAssuredPojo/pojos/PokeAPI_FlavorPojo.java
@@ -0,0 +1,10 @@
+package RestAssuredPojo.pojos;
+
+import lombok.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PokeAPI_FlavorPojo {
+    private PokeAPI_FlavorPojo flavorPojo;
+    private int potency;
+}
