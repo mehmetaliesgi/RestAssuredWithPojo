97753853e77611b17173d348ad8c6b827fb3bf96
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-06-27 12:28:13 +0300

    NaturalGiftTypePojo files was added the project using to lombok library

---
+++ untitled/src/test/java/RestAssuredPojo/pojos/PokeAPI_NaturalGiftTypePojo.java
@@ -0,0 +1,11 @@
+package RestAssuredPojo.pojos;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PokeAPI_NaturalGiftTypePojo {
+    private String name;
+    private String url;
+}
