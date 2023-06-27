9df0c9adcc1d0ad74270fcb508e322c7173402f2
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-06-27 12:26:24 +0300

    ItemPojo files was added the project using to lombok library

---
+++ untitled/src/test/java/RestAssuredPojo/pojos/PokeAPI_ItemPojo.java
@@ -0,0 +1,11 @@
+package RestAssuredPojo.pojos;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PokeAPI_ItemPojo {
+    private String name;
+    private String url;
+}
