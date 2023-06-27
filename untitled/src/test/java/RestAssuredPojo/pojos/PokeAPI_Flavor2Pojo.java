721fd18dd33426e5b554bdc303b747ab109de130
mehmetaliesgi <mehmetaliesgi@gmail.com>
2023-06-27 12:24:52 +0300

    Flavor2Pojo files was added the project using to lombok library

---
+++ untitled/src/test/java/RestAssuredPojo/pojos/PokeAPI_Flavor2Pojo.java
@@ -0,0 +1,13 @@
+package RestAssuredPojo.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PokeAPI_Flavor2Pojo {
+    private String name;
+    private String url;
+}
